3698dd2ba8c57bfda5d5da7dd4c87c24c4552b4d
EdRRojas <20220847@itla.edu.do>
2024-07-08 23:38:46 -0400

    Agregar archivos de proyecto.

---
+++ PublisherProject/Publisher.cs
@@ -0,0 +1,33 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+class Publisher
+{
+    private static ConnectionMultiplexer? redis;
+    private static ISubscriber? sub;
+
+    static async Task Main(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        var redisHost = configuration["Redis:Host"];
+        var redisPort = configuration["Redis:Port"];
+        var redisPassword = configuration["Redis:Password"];
+
+        var redisConnectionString = $"{redisHost}:{redisPort},password={redisPassword}";
+        redis = await ConnectionMultiplexer.ConnectAsync(redisConnectionString);
+        sub = redis.GetSubscriber();
+
+        Console.Write("Escribe el mensaje a enviar: ");
+        var message = Console.ReadLine();
+
+        await sub.PublishAsync(new RedisChannel("messages", RedisChannel.PatternMode.Literal), message);
+        Console.WriteLine($"Mensaje '{message}' publicado satisfactoriamente.");
+    }
+}
---
+++ SusbcriberProject/Subscriber.cs
@@ -0,0 +1,34 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+class Subscriber
+{
+    private static ConnectionMultiplexer? redis;
+    private static ISubscriber? sub;
+
+    static async Task Main(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        var redisHost = configuration["Redis:Host"];
+        var redisPort = configuration["Redis:Port"];
+        var redisPassword = configuration["Redis:Password"];
+
+        var redisConnectionString = $"{redisHost}:{redisPort},password={redisPassword}";
+        redis = await ConnectionMultiplexer.ConnectAsync(redisConnectionString);
+        sub = redis.GetSubscriber();
+
+        await sub.SubscribeAsync(new RedisChannel("messages", RedisChannel.PatternMode.Literal), (channel, message) => {
+            Console.WriteLine($"Mensaje Recivido: {message}");
+        });
+
+        Console.WriteLine("Suscrito al canal de 'mensajes'. Precione cualquier boton para cancelar la suscripción...");
+        Console.ReadKey();
+    }
+}
